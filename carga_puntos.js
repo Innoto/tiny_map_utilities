abf7421ca7bd2b4a31b13ab37ca9df263024e1e1
pblanco <pblanco@H2O2>
2014-02-03 16:40:32 +0100

    Convertido nun obxeto

--- carga_puntos.js
+++ carga_puntos.js
@@ -1,20 +1,12 @@
-var zoom_range= [12,17];
-var group_radious = 10; // in pixels
-var json_points = new Array();
-var r_trees = new Array();
-var raw_cluster_array = Array();
+
+var json_data = false;
 var mapa = false;
 
-tiempo_total= new time_calc();
 
+
+tiempo_total= new time_calc();
 $(document).ready(function() {
 
-
-
-  // load json
-  carga_json= new time_calc();
-  load_json();
-  $("#carga_json").text( carga_json.check() );
 
 
   // gmaps init
@@ -22,20 +14,34 @@ $(document).ready(function() {
   load_gmap();
 
   google.maps.event.addListenerOnce(mapa, 'idle', function( ){
+
     $("#gmap_load").text( gmap_load.check() );  
 
+
+
+    // load json
+    carga_json= new time_calc();
+    load_json();
+    $("#carga_json").text( carga_json.check() );
+
+    cluster_manager =  new MarkerClusterer_v3({
+      map: mapa
+    })
+
+    cluster_manager.load_data(json_data);
+    
+
     carga_arbol= new time_calc();
-    create_r_trees();
+    cluster_manager.create_r_trees();
     $("#carga_arbol").text( carga_arbol.check() );
 
     procesado_clusters= new time_calc();
-    raw_cluster_points();
+    cluster_manager.raw_cluster_points();
     $("#procesado_clusters").text( procesado_clusters.check() );
+
 
     carga_sobre_gmaps= new time_calc();
     $("#carga_sobre_gmaps").text( carga_sobre_gmaps.check() );
-
-
     $("#tiempo_total").text( tiempo_total.check() );
 
   });
@@ -48,10 +54,7 @@ function load_json() {
     type: 'get',
     cache: false,
     success: function(data) { 
-
-      $(data).each( function(i,e) {
-        json_points[i] = e;
-      });
+      json_data = data;
     },
     async:false,
   });
@@ -59,7 +62,7 @@ function load_json() {
 
 function load_gmap() {
   var mapOptions = {
-    zoom: zoom_range[0],
+    zoom: 11,
     center: new google.maps.LatLng(
       51.511688,
       -0.185094
@@ -70,48 +73,6 @@ function load_gmap() {
 }
 
 
-
-function create_r_trees() {
-
-  mapProjection = mapa.getProjection();
-
-  // init or reset r trees
-  for(zoomlevel = zoom_range[0]; zoomlevel<=zoom_range[1] ;zoomlevel++) {
-    r_trees[zoomlevel] = rbush($(json_points).length, ['.lat', '.lng', '.lat', '.lng']);
-  }
-
-  $(json_points).each(function(i,e){
-    for(zoomlevel = zoom_range[0]; zoomlevel<=zoom_range[1] ;zoomlevel++) {
-      var scale = Math.pow(2, zoomlevel);
-      pixels_latlng =  mapProjection.fromLatLngToPoint( new google.maps.LatLng(e.latitude, e.longitude) );
-      tree_row = { id: e.id, lat: parseInt(pixels_latlng.x*scale) , lng: parseInt(pixels_latlng.y*scale) };
-      r_trees[zoomlevel].insert( tree_row );
-    }
-    //console.debug('lat:'+ parseInt(pixels_latlng.x*scale) + 'lng: '+parseInt(pixels_latlng.y*scale) );
-  });
-  
-}
-
-
-function raw_cluster_points() {
-
-  for(zoomlevel = zoom_range[0]; zoomlevel<=zoom_range[1] ;zoomlevel++) {
-
-    raw_cluster_array[zoomlevel] = [];
-    $( r_trees[zoomlevel].data.children ).each(function(i,e){
-
-      var result = r_trees[zoomlevel].search([ e.lat - group_radious, e.lng - group_radious, e.lat + group_radious, e.lng + group_radious]);
-
-      var group= [];
-      $( result ).each( function(i,ee){
-          group.push(ee.id);
-      });
-      
-      raw_cluster_array[zoomlevel][e.id] = group;
-
-    });
-  }
-}
 
 
 
