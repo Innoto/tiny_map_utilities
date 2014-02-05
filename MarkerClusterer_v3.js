88fc9d7844d8b5c9413097853529a0b0747716f3
pblanco <pablinho.blanco@gmail.com>
2014-02-05 15:37:10 +0100

    con arrays binarios a ver se vai mellor

--- MarkerClusterer_v3.js
+++ MarkerClusterer_v3.js
@@ -5,8 +5,10 @@ function  MarkerClusterer_v3( opts ) {
   this.json_data = false;
   this.json_points = [];
   this.r_trees = [];
+  this.raw_cluster_array_keys = [];
   this.raw_cluster_array = [];
-  this.cluster_array_tmp = [];
+  this.cluster_array_keys = [];
+  this.cluster_array_tmp_keys = [];
   this.cluster_array = [];
 
   
@@ -63,97 +65,79 @@ function  MarkerClusterer_v3( opts ) {
     for(zoomlevel = this.options.zoom_range[0]; zoomlevel<=this.options.zoom_range[1] ;zoomlevel++) {
 
       that.raw_cluster_array[zoomlevel] = [];
-
+      that.raw_cluster_array_keys[zoomlevel] = [];
       $( that.r_trees[zoomlevel].data.children ).each(function(i,e){
 
         var result = that.r_trees[zoomlevel].search([ e.lat - that.options.group_radious, e.lng - that.options.group_radious, e.lat + that.options.group_radious, e.lng + that.options.group_radious]);
 
-        group = [];
+        var group = [];
         $( result ).each( function(i,ee){
             group.push(ee.index);
         });
 
-        that.raw_cluster_array[zoomlevel].push(group);
+        that.raw_cluster_array[zoomlevel][i] = new Int32Array(group);
+        that.raw_cluster_array_keys[zoomlevel].push(i);
 
       });
 
     }
-  };
+  }
 
 
   this.cluster_points = function( filter ) {
+    var  biggest_cluster;
+    this.cluster_array = this.raw_cluster_array;
+    this.cluster_array_keys = this.raw_cluster_array_keys;
+    this.cluster_array_tmp_keys = this.raw_cluster_array_keys;
 
-    var biggest_cluster;
-    that = this;
-    this.cluster_array_tmp = this.raw_cluster_array;
-
-
-    for(var zoomlevel = this.options.zoom_range[0]; zoomlevel<=this.options.zoom_range[1] ;zoomlevel++) {
-      
-      this.cluster_array[zoomlevel] = [];
-
-      while( this.cluster_array_tmp[zoomlevel].length != 0) {
-        
-          // get biggest cluster index
-          biggest_cluster_index= this.biggest_cluster_index(this.cluster_array_tmp[ zoomlevel ]);
-          
-          biggest_cluster = this.cluster_array_tmp[zoomlevel][biggest_cluster_index];
-
-          // push biggest tmp cluster to cluster array
-          this.cluster_array[zoomlevel].push(biggest_cluster);
-
-
-          // delete cluster 0 form tmp array
-          if(this.cluster_array_tmp[zoomlevel].length > 1)
-            this.cluster_array_tmp[zoomlevel].splice(biggest_cluster_index,1);
-          else
-            this.cluster_array_tmp[zoomlevel]=[];
-          
-          // deleting duplicates acording with first_cluster
-          if( biggest_cluster.length > 1)
-            this.cluster_array_tmp[zoomlevel] = this.clean_clusters(this.cluster_array_tmp[zoomlevel], biggest_cluster);
-
-        }
-
+    for(zoomlevel = this.options.zoom_range[0]; zoomlevel<=this.options.zoom_range[1] ;zoomlevel++) {
+      while(this.cluster_array_tmp_keys[zoomlevel].length > 0){
+        //console.debug(this.cluster_array_tmp_keys[zoomlevel].length);
+        biggest_cluster_index = this.biggest_cluster_index(this.cluster_array_tmp_keys[zoomlevel], this.cluster_array[zoomlevel]);
+        this.cluster_array_tmp_keys[zoomlevel].splice(biggest_cluster_index, 1);
+        this.clean_clusters( zoomlevel , this.cluster_array[zoomlevel][biggest_cluster_index]);
+      }
     }
+  }
 
 
+  this.clean_clusters = function(zoom, cluster_to_compare) {
+    that = this;
 
-  };
+    $(that.cluster_array_tmp_keys[zoom]).each(function(i,e){
+      if( $.inArray(e, cluster_to_compare)  !== 1 ) {
+        that.cluster_array_keys[zoom].splice(e,1)
+        that.cluster_array_tmp_keys[zoom].splice(e,1)
+      }
 
 
-  this.clean_clusters = function(point_clusters, cluster_to_compare) {
-
-    var res_clusters = [];
-    var cluster = []
-    $(point_clusters).each( function(i,e){
-
-      cluster = []
-
-      $(e).each( function(i2,e2){
-        if( $.inArray( e2, cluster_to_compare ) === -1 ) {
-          cluster.push(e2);
+      var group = [];
+      $(that.cluster_array[zoomlevel][e]).each(function(i2,e2){ 
+        if( $.inArray(e2, cluster_to_compare)  === 1) {
+          group.push(e2);
         }
       });
 
-      if(cluster.length>0) {
-        res_clusters.push(cluster);
+      if(group.length != 0){
+        that.cluster_array[zoom][i] = new Int32Array(group);
+      }
+      else{
+        that.cluster_array_keys[zoom].splice(e,1)
+        that.cluster_array_tmp_keys[zoom].splice(e,1)
       }
 
     });
 
-    return res_clusters;
   }
 
-  this.biggest_cluster_index = function(point_clusters){
+  this.biggest_cluster_index = function(point_cluster_keys, point_cluster_array){
     var biggest_cluster_length=0;
     var biggest_cluster_index=0;
 
-    $(point_clusters).each(function(i,e){
-
-      if(e.length > biggest_cluster_length) {
+    $(point_cluster_keys).each(function(i,e){
+      if(point_cluster_array[e].length > biggest_cluster_length) {
         biggest_cluster_index=i;
-        biggest_cluster_length = e.length;
+        biggest_cluster_length = point_cluster_array[e].length;
       }
     });
 
@@ -162,68 +146,9 @@ function  MarkerClusterer_v3( opts ) {
 
 
 
-
   this.draw_markers = function() {
 
-    //
-    //  Establecer zooms de novo
-    //  Visibilidade null para todos
-    //  Reiniciar zoom mínimo a zoom máximo
-    //
-
-    that = this;
-    var marker;
-    var marker_latlng;
-
-    //for(var zoomlevel = this.options.zoom_range[0]; zoomlevel<=this.options.zoom_range[1] ;zoomlevel++) {
-      //console.debug("zoom:"+zoomlevel);
-      //console.debug("elementos:"+that.cluster_array[zoomlevel].length);
-      $(that.cluster_array[17]).each(function( i,cc ){
-
-        //console.debug("subelementos:" + cc.length);
-        console.debug(cc);
-
-/*
-        marker_latlng = new google.maps.LatLng( that.json_points[cc[0]].latitude, that.json_points[cc[0]].longitude );
-
-        marker = new google.maps.Marker({
-          position: marker_latlng,
-          map: that.options.map,
-          
-          visible:true
-        });
-
-*/
-
-        //console.debug(    i + ' _ ' +that.json_points[cc[0]].id + ' = ' +that.json_points[cc[0]].latitude +','+ that.json_points[cc[0]].longitude     );
-
-        //console.debug(that.json_points[cc[0]].latitude  );
-
-      });
-    //}
-
-
-/*
-    for (var zoomlevel = 0; zoomlevel <= maxzoom; zoomlevel++) {
-
-      $(this.point_clusters[zoomlevel]).each(function(i, cluster){
-        if( $(cluster).length == 1 ) {
-
-          // parámetros por extender
-          this.markers[i].zoom_minimo = zoomlevel;
-          this.markers[i].visible = true;
-
-        }
-      });
-    }*/
-
   }
-
-
-  this.remove_markers = function() {
-
-  }
-
 
 
 }
