75dd3b9ac7e3ab4a7df23962af11ee3cb610c2b8
pblanco <pablinho.blanco@gmail.com>
2014-02-17 19:04:37 +0100

    hello world en overlay

--- smart_infowindow/smart_infowindow.js
+++ smart_infowindow/smart_infowindow.js
@@ -1,51 +1,45 @@
 
+var overlay;
+smart_infowindow.prototype = new google.maps.OverlayView();
 
+
+/** @constructor */
 function smart_infowindow(opts) {
 
-  s_i_that = this;
-  this.prototype = new google.maps.OverlayView();
-  this.prototype.constructor = this;
   this.options = new Object({
     map : false,
-
   });
   $.extend(true, this.options, opts);
 
-
-
-  // init
-  google.maps.event.addListenerOnce(this.options.map, 'idle', function( ){
-    s_i_that.setMap(this.options.map);
-    s_i_that.create_infowindow();
-  });
-
-
-  this.create_infowindow = function() {
-
-    var panes = this.getPanes();
-
-    this.infowindow = document.createElement('div');
-    panes.overlayLayer.appendChild(this.infowindow);
-  }
-
-  this.open = function( marker ) {
-
-    var overlayProjection = this.getProjection();
-
-    var point = overlayProjection.fromLatLngToDivPixel(marker);
-
-    $(this.infowindow).css('display' , 'true');
-    $(this.infowindow).css('position' , 'absolute');
-    $(this.infowindow).css('left', point.x + 'px');
-    $(this.infowindow).css('top', point.y + 'px');
-    $(this.infowindow).css('width', '100px';
-    $(this.infowindow).css('height', '100px' ); 
-
-  }
-
-  this.close = function() {
-    $(this.infowindow).css('display' , 'none');
-  }
-
-
+  this.div_ = null;
+  this.setMap(this.options.map);
 }
+
+
+smart_infowindow.prototype.onAdd = function() {
+
+  var div = document.createElement('div');
+  div.style.borderStyle = 'none';
+  div.style.borderWidth = '0px';
+  div.style.position = 'absolute';
+
+  $(div).html("<div style='background-color:white;'>ola mundo</div>");
+
+  this.div_ = div;
+
+  // Add the element to the "overlayLayer" pane.
+  var panes = this.getPanes();
+  panes.overlayLayer.appendChild(div);
+
+  var overlayProjection = this.getProjection();
+
+  var div = this.div_;
+  div.style.left = 15 + 'px';
+  div.style.top = 200 + 'px';
+};
+
+smart_infowindow.prototype.draw = function() {
+};
+
+
+
