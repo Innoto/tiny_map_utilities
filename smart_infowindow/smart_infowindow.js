4f731356e34b90164890deefe400d7b3239eda90
pblanco <pablinho.blanco@gmail.com>
2014-02-18 16:53:05 +0100

    pico

--- smart_infowindow/smart_infowindow.js
+++ smart_infowindow/smart_infowindow.js
@@ -180,6 +180,7 @@ smart_infowindow.prototype.SetPosition = function( marker, click_ev ) {
 
 smart_infowindow.prototype.SetPeak = function(v, h) {
 
+
   var peak_img = document.createElement('img');
   $(peak_img).attr('src', this.options.peak_img);
 
